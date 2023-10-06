432de1e2de442d357a16e8b39c2d365a1276c8d6
Brandon Perfetti <brandon@brandonperfetti.com>
2023-10-06 10:27:33 -0700

    Generate TypeScript Definitions from PostgreSQL Schema with Supabase CLI.

--- app/auth-button-client.tsx
+++ app/auth-button-client.tsx
@@ -6,8 +6,12 @@ import {
 } from '@supabase/auth-helpers-nextjs'
 import { useRouter } from 'next/navigation'
 
-export default function AuthButtonClient({ session }: { session: Session | null }) {
-	const supabase = createClientComponentClient()
+export default function AuthButtonClient({
+	session,
+}: {
+	session: Session | null
+}) {
+	const supabase = createClientComponentClient<Database>()
 	const router = useRouter()
 
 	const handleSignOut = async () => {
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,18 @@
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
-import AuthButtonServer from './auth-button-server'
 import { redirect } from 'next/navigation'
+import AuthButtonServer from './auth-button-server'
 
 export default async function Home() {
-	const supabase = createServerComponentClient({ cookies })
+	const supabase = createServerComponentClient<Database>({ cookies })
 
-	const {data: {session}} = await supabase.auth.getSession()
+	const {
+		data: { session },
+	} = await supabase.auth.getSession()
 	if (!session) {
 		redirect('/login')
 	}
-	
+
 	const { data: tweets } = await supabase.from('tweets').select()
 
 	return (
