2c1001a54e22c451321e37f56e858a300b3c8330
Brandon Perfetti <brandon@brandonperfetti.com>
2023-10-06 17:57:14 -0700

    Dynamically update UI with Database Changes using Supabase Realtime.

--- app/tweets.tsx
+++ app/tweets.tsx
@@ -1,28 +1,55 @@
-'use client'
+"use client";
 
-import { experimental_useOptimistic as useOptimistic } from 'react'
-import Likes from './likes'
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useRouter } from "next/navigation";
+import { useEffect, experimental_useOptimistic as useOptimistic } from "react";
+import Likes from "./likes";
 
 export default function Tweets({ tweets }: { tweets: TweetWithAuthor[] }) {
-	const [optimisticTweets, addOptimisticTweet] = useOptimistic<
-		TweetWithAuthor[],
-		TweetWithAuthor
-	>(tweets, (currentOptimisticTweets, newTweet) => {
-		const newOptimisticTweets = [...currentOptimisticTweets]
-		const index = newOptimisticTweets.findIndex(
-			tweet => tweet.id === newTweet.id,
-		)
-		newOptimisticTweets[index] = newTweet
-		return newOptimisticTweets
-	})
+  const [optimisticTweets, addOptimisticTweet] = useOptimistic<
+    TweetWithAuthor[],
+    TweetWithAuthor
+  >(tweets, (currentOptimisticTweets, newTweet) => {
+    const newOptimisticTweets = [...currentOptimisticTweets];
+    const index = newOptimisticTweets.findIndex(
+      (tweet) => tweet.id === newTweet.id
+    );
+    newOptimisticTweets[index] = newTweet;
+    return newOptimisticTweets;
+  });
 
-	return optimisticTweets.map(tweet => (
-		<div key={tweet.id}>
-			<p>
-				{tweet.author.name} {tweet.author.username}
-			</p>
-			<p>{tweet.title}</p>
-			<Likes tweet={tweet} addOptimisticTweet={addOptimisticTweet} />
-		</div>
-	))
+  const supabase = createClientComponentClient();
+  const router = useRouter();
+
+  useEffect(() => {
+    const channel = supabase
+      .channel("realtime tweets")
+      .on(
+        "postgres_changes",
+        {
+          event: "*",
+          schema: "public",
+          table: "tweets",
+        },
+        (payload) => {
+          console.log({ payload })
+          router.refresh();
+        }
+      )
+      .subscribe();
+
+    return () => {
+      supabase.removeChannel(channel);
+    };
+  }, [supabase, router]);
+
+  return optimisticTweets.map((tweet) => (
+    <div key={tweet.id}>
+      <p>
+        {tweet.author.name} {tweet.author.username}
+      </p>
+      <p>{tweet.title}</p>
+      <Likes tweet={tweet} addOptimisticTweet={addOptimisticTweet} />
+    </div>
+  ));
 }
